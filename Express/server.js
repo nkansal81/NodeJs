3c858a9597a4f7235a621532d16698c1757e31c8
Nitika <nkansal81@gmail.com>
2021-10-16 01:22:14 +0530

    Update and rename server_post.js to server.js

--- Express/server.js
+++ Express/server.js
@@ -4,11 +4,11 @@ var bodyParser = require('body-parser');
 // Create application/x-www-form-urlencoded parser  
 var urlencodedParser = bodyParser.urlencoded({ extended: false })  
 // app.use(express.static('public'));  
-app.get('/index.html', function (req, res) {  
+app.get('/user', function (req, res) {  
    res.sendFile( __dirname + "/" + "index.html" );  
 })  
 
-app.post('/process_post', urlencodedParser,function (req, res) {  
+app.post('/user', urlencodedParser,function (req, res) {  
    // Prepare output in JSON format  
    response = {  
        first_name:req.query.first_name,  
@@ -17,24 +17,6 @@ app.post('/process_post', urlencodedParser,function (req, res) {
    console.log(response);  
    res.end(JSON.stringify(response));  
 }) 
-
-app.delete('/deleteUser/:id', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-      data = JSON.parse( data );
-      // const id = req.params.id;
-      const user = data.indexOf(p=> p.id === id)
-      // console.log(data);
-      // var user = data["user" + id];
-      // console.log(user)
-      // data.splice((id-1),1);
-      delete user;
-      // delete data["user" + req.params.id];
-       
-      // console.log( data );
-      res.end( JSON.stringify(data));
-   });
-})
 
 var server = app.listen(8000, function () {  
   var host = server.address().address  
